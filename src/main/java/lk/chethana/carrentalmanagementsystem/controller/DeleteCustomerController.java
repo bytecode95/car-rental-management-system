10d7446d71593bea7978388099a9b35e30a1f55c
chethanavirajini <cheth.virajini@gmail.com>
2023-12-17 15:00:36 +0530

    updated

---
+++ src/main/java/lk/chethana/carrentalmanagementsystem/controller/DeleteCustomerController.java
@@ -0,0 +1,24 @@
+package lk.chethana.carrentalmanagementsystem.controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+
+public class DeleteCustomerController {
+    @FXML
+    private AnchorPane root;
+
+    @FXML
+    private TextField txtClientID;
+
+    @FXML
+    void onCancel(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onDelete(ActionEvent event) {
+
+    }
+}
